610eaef4e6f8b718ab45ed7f0cad68c10be0ea3c
Mohammed Ali Abd ElShaheed <engineer.mohammed.ali11@gmail.com>
2026-04-08 12:34:16 +0200

    start shop.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// #region navbar
+//#region navbar & footer
 
 let navbar = `    <!-- start navbar -->
     <nav class="navbar navbar-expand-lg p-0 position-fixed shadow-lg z-3">
@@ -77,9 +77,174 @@ let navbar = `    <!-- start navbar -->
             </div>
         </div>
     </nav>`
-    ;
+;
 
-document.body.innerHTML = navbar + document.body.innerHTML;
+//#region footer
+let footer = `<!--#region footer -->
+<footer class="bg-dark text-white">
+
+    <div class="container">
+
+        <div class="content d-flex justify-content-center p-5">
+
+            <div class="f-pages w-25 d-flex align-items-center flex-column">
+
+                <div class="logo">
+
+                    <a href="" class=""><img class="img-fluid" src="img/assets/images/logo.png" alt="logo"></a>
+
+                </div>
+
+                <div class="links-box w-100 d-flex justify-content-between align-items-center p-3">
+
+                    <div class="left-side d-flex justify-content-center align-items-start flex-column">
+
+                        <a href="home.html" class="link w-100 fw-bold p-1 hvr-forward">Home</a>
+
+                        <a href="shop.html" class="link w-100 fw-bold p-1 hvr-forward">Shop</a>
+
+                        <a href="about_us.html" class="link w-100 fw-bold p-1 hvr-forward">About Us</a>
+
+                    </div>
+
+                    <div class="right-side d-flex justify-content-center align-items-start flex-column">
+
+                        <a href="contact.html" class="link w-100 fw-bold p-1 hvr-forward">Contact Us</a>
+
+                        <a href="wishlist" class="link w-100 fw-bold p-1 hvr-forward">Wishlist</a>
+
+                        <a href="Cart.html" class="link w-100 fw-bold p-1 hvr-forward">Cart</a>
+
+                    </div>
+
+                </div>
+
+                <div class="other-sites w-100 d-flex align-items-center justify-content-between p-3 border">
+
+                    <a href="https://youtube.com" target="_blank"><i class="fa-brands fa-youtube fs-5 p-2 hvr-grow"></i></a>
+
+                    <a href="https://facebook.com" target="_blank"><i
+                            class="fa-brands fa-square-facebook fs-5 p-2 hvr-grow"></i></a>
+
+                    <a href="https://x.com" target="_blank"><i class="fa-brands fa-x-twitter fs-5 p-2 hvr-grow"></i></a>
+
+                    <a href="https://instagram.com" target="_blank"><i
+                            class="fa-brands fa-instagram fs-5 p-2 hvr-grow"></i></a>
+
+                </div>
+
+            </div>
+
+            <div class="f-categories w-25 d-flex align-items-start flex-column ps-5">
+
+                <div class="title mt-4">
+
+                    <h2 class="ms-3">Categories:</h2>
+
+                </div>
+
+                <div class="links-box w-100 d-flex justify-content-between align-items-center p-3">
+
+                    <div class="left-side d-flex justify-content-center align-items-start flex-column">
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate1</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate2</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate3</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate4</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate5</a>
+
+                    </div>
+
+                    <div class="right-side d-flex justify-content-center align-items-start flex-column">
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate8</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate9</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate10</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate11</a>
+
+                        <a href="" class="link w-100 fw-bold p-1 hvr-forward">Cate12</a>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <div class="f-account w-25 d-flex align-items-start flex-column ps-5">
+
+                <div class="title mt-4 ms-2">
+
+                    <h2 class="ms-2">My Account:</h2>
+
+                </div>
+
+                <div class="links-box w-100 d-flex flex-column justify-content-between align-items-center p-3">
+
+                    <a href="" class="link w-100 fw-bold p-1 hvr-forward">My Account</a>
+
+                    <a href="" class="link w-100 fw-bold p-1 hvr-forward">Sign In</a>
+
+                    <a href="" class="link w-100 fw-bold p-1 hvr-forward">Sign Up</a>
+
+                    <a href="" class="link w-100 fw-bold p-1 hvr-forward">Check Out</a>
+
+                    <a href="" class="link w-100 fw-bold p-1 hvr-forward">FAQ</a>
+
+
+                </div>
+
+            </div>
+
+            <div class="information w-25 d-flex align-items-start flex-column ps-5">
+
+                <div class="title mt-4 ms-2">
+
+                    <h2 class="ms-2">Information:</h2>
+
+                </div>
+
+                <p class="w-100 p-3">
+                    ABC, Address Here, Country <br>
+                    Call Us: <a href="tel:01210908070" target="_blank">01210908070</a><br>
+                    Email: <a href="https://mail.google.com" target="_blank">engineer.mohammed.ali11@gmail.com</a>
+                </p>
+
+                <a href="https://maps.google.com" target="_blank"><img class="img-fluid ps-3" src="img/download.jpg"
+                                                                       alt=""></a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
+<!--#endregion footer -->`;
+let copyright = `<!--#region copyright -->
+<div class="copyright">
+
+    <div class="container w-100 h-100">
+
+        <p class="ms-5 w-100 h-100 d-flex align-items-center justify-content-between">
+            © 2025 memo All rights reserved.
+        </p>
+
+    </div>
+
+</div>
+<!--#endregion copyright -->`
+//#endregion footer
+
+document.body.innerHTML = navbar + document.body.innerHTML + footer + copyright;
+
 let links = [document.getElementsByClassName("nav-link")[0], document.getElementsByClassName("nav-link")[1], document.getElementsByClassName("nav-link")[2], document.getElementsByClassName("nav-link")[3], document.getElementsByClassName("nav-link")[4]];
 let links2 = [document.getElementsByClassName("acount-link")[0], document.getElementsByClassName("acount-link")[1], document.getElementsByClassName("acount-link")[2], document.getElementsByClassName("icon-heart")[0], document.getElementsByClassName("icon-shop")[0]];
 
@@ -89,7 +254,8 @@ links.forEach((e) => {
     if (window.location.href === e.href) {
         e.classList.remove("hvr-underline-from-center");
         e.classList.add("active");
-    };
+    }
+    ;
 });
 
 links2.forEach((e) => {
@@ -99,8 +265,10 @@ links2.forEach((e) => {
         if (e.innerHTML === "My Account" || e.innerHTML === "Sign In" || e.innerHTML === "Check Out") {
             document.getElementsByClassName("icon-account")[0].style.background = "var(--bg-color) !important";
             document.getElementsByClassName("icon-account")[0].style.color = "var(--main-color) !important";
-        };
-    };
+        }
+        ;
+    }
+    ;
 });
 
 //search bar
@@ -142,11 +310,11 @@ document.getElementsByClassName("account-collapse")[0].addEventListener("click",
     document.getElementsByClassName("icon-account")[0].style.color = "var(--main-color) !important";
 });
 
-//#endregion
+//#endregion nav links access
 
-//#endregion navbar
+//#endregion navbar & footer
 
-// #region landing
+//#region landing
 
 let leftArrow = document.getElementsByClassName("arrow-left")[0];
 let rightArrow = document.getElementsByClassName("arrow-right")[0];
@@ -190,7 +358,7 @@ rightArrow.addEventListener("click", () => {
 
 //#endregion landing
 
-// #region new collections
+//#region new collections
 
 let daysInp = document.getElementsByClassName("days")[0];
 let hoursInp = document.getElementsByClassName("hours")[0];
@@ -202,10 +370,10 @@ setInterval(() => {
     let timeNow = new Date();
     let timeDef = time - timeNow;
 
-    // days
+    //days
     let days = Math.floor((timeDef / (1000 * 60 * 60 * 24)));
     daysInp.innerHTML = days < 10 ? `0${days}` : days;
-    // hours
+    //hours
     let hours = Math.floor((timeDef % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     hoursInp.innerHTML = hours < 10 ? `0${hours}` : hours;
     //minutes
@@ -215,8 +383,8 @@ setInterval(() => {
     let seconds = Math.floor((timeDef % (1000 * 60)) / (1000));
     secondsInp.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
 
-    // conditions
-    // style condition
+    //conditions
+    //style condition
     if (daysInp.innerHTML === "00") {
 
         daysInp.classList.add("text-danger");
@@ -238,19 +406,20 @@ setInterval(() => {
         hoursInp.classList.remove("text-danger");
 
     }
-    // clear interval condition
+    //clear interval condition
     if (timeDef === 0) {
         clearInterval();
-    };
+    }
+    ;
 }, 1000);
 
-// #endregion new collections
+//#endregion new collections
 
-// #region featured products
+//#region featured products
 let productsBox = document.querySelector(".featured-products .products-box");
 let leftAngle = document.querySelector(".featured-products .left-arrow");
 let rightAngle = document.querySelector(".featured-products .right-arrow");
-// code
+//code
 
 leftAngle.addEventListener("click", () => {
     let product = productsBox.firstElementChild;
@@ -265,13 +434,13 @@ rightAngle.addEventListener("click", () => {
     productsBox.prepend(product);
 });
 
-// #endregion featured products
+//#endregion featured products
 
-// #region featured products
+//#region featured products
 let sProductsBox = document.querySelector(".sale-products .products-box");
 let sLeftAngle = document.querySelector(".sale-products .left-arrow");
 let sRightAngle = document.querySelector(".sale-products .right-arrow");
-// code
+//code
 
 sLeftAngle.addEventListener("click", () => {
     let product = sProductsBox.firstElementChild;
@@ -286,4 +455,4 @@ sRightAngle.addEventListener("click", () => {
     sProductsBox.prepend(product);
 });
 
-// #endregion featured products
+//#endregion featured products
